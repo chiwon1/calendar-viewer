11044aa45342acbaee08b87afd33e680ee589514
Chiwon Choi <emphasize1@gmail.com>
2021-07-22 20:20:45 +0900

    Feat: build daily calendar static version

--- src/common/components/Calendar/Daily/Daily.js
+++ src/common/components/Calendar/Daily/Daily.js
@@ -1,29 +1,49 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  .calendar-container {
+    width: 1200px;
+  }
+
+  .top-row-container, .row-container  {
+    display: flex;
+  }
+
+  .first-column {
+    width: 30%;
+    height: 50px;
+    border: 1px solid black;
+  }
+
+  .second-column {
+    width: 70%;
+    height: 50px;
+    border: 1px solid black;
+  }
+`;
 
 function Daily() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>Time</td>
-          <td>Event</td>
-        </tr>
-      </thead>
-      <tbody>
+    <Wrapper>
+      <div className="calendar-container">
+        <div className="top-row-container">
+          <div className="first-column">Time</div>
+          <div className="second-column">Event name</div>
+        </div>
+        <div>
         {Array.from(Array(24).keys()).map((hour) => (
-          <>
-            <tr>
-              <td>{`${hour}:00 - ${hour}:30`}</td>
-              <td>Event 1</td>
-            </tr>
-            <tr>
-              <td>{`${hour}:30 - ${hour + 1}:00`}</td>
-              <td>Event 2</td>
-            </tr>
-          </>
+          <div className="row-container">
+            <div className="first-column">{`${hour}:00 - ${hour + 1}:00`}</div>
+            <div
+              className="second-column"
+            >
+            </div>
+          </div>
         ))}
-      </tbody>
-    </table>
+        </div>
+      </div>
+    </Wrapper>
   );
 }
 
